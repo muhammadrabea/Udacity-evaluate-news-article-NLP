20119ea78366c383fe334764a703bdba139dd519
muhammadrabea <77594974+muhammadrabea@users.noreply.github.com>
2021-08-26 17:57:15 +0200

    Update testHandleSubmit.spec.js

--- src/client/__test__/testHandleSubmit.spec.js
+++ src/client/__test__/testHandleSubmit.spec.js
@@ -1,8 +1,9 @@
-import "babel-polyfill";
+import 'babel-polyfill'
 
-import { handleSubmit } from "../js/formHandler"
+import handleSubmit from '../js/formHandler'
 
-describe("Testing the submit functionality", () => {
-  test("Testing the handleSubmit() function", () => {
-         expect(handleSubmit).toBeDefined();
-})});
+describe('Testing the submit functionality', () => {
+  test('Testing the handleSubmit() function', () => {
+    expect(handleSubmit).toBeDefined()
+  })
+})
